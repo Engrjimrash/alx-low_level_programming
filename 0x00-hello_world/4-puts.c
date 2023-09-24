9627390c2e0cb16c5282b39861ce61cb072a093a
jimohrasheed <rastopsenior6@gmail.com>
2023-09-24 15:37:06 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success) 
+ *Description:A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
 */
 int main(void)
-{ 	
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{
+	puts("Programming is like building a multilingual puzzle");
+     	return (0);
 }
